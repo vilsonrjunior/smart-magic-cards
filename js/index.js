33ee7ca7182274f7e8dfc210c0b1de0dde46e6cc
vilson junior <vilsonrjunior@gmail.com>
2019-11-24 18:00:43 +0000

    1st commit with basic requirements completed

--- js/index.js
+++ js/index.js
@@ -1,38 +1,88 @@
-const suit = 'hearts';
 const cardsWrapper = document.querySelector('.cards-wrapper');
+const suits = ['hearts', 'spades', 'diamonds', 'clubs'];
 
+// creates cards by passing each suit through the loop and push card to cards array
 function createCards() {
   const cards = [];
-  // Create an array with objects containing the value and the suit of each card
-  for (let i = 1; i <= 13; i += 1) {
-    const cardObject = {
-      value: i,
-      suit,
-    };
-    cards.push(cardObject);
-  }
-
-  // For each dataObject, create a new card and append it to the DOM
-  cards.forEach((card, i) => {
-    const positionFromLeft = i * 15;
+  suits.forEach((suit) => {
+    for (let i = 1; i <= 13; i += 1) {
+      const cardObject = {
+        value: i,
+        suit,
+      };
+      cards.push(cardObject);
+    }
+  });
+  return cards;
+}
+// reusable func to create arranged and shuffled decks
+function createDecks(deck) {
+  deck.forEach((card, i) => {
+    const positionFromLeft = i * 25;
     const cardElement = document.createElement('div');
     cardElement.setAttribute('data-value', card.value);
+    cardElement.setAttribute('id', 'cards');
     cardElement.classList.add('card', `${card.suit}-${card.value}`);
     cardElement.style.left = `${positionFromLeft}px`;
     cardsWrapper.append(cardElement);
   });
 }
-
-// Function to clear out the initial button and create new buttons to play the game.
-function createButtons() {
-  // Your Code
+// adds hidden css class to div
+function hideShow() {
+  cardsWrapper.classList.toggle('hidden');
 }
 
-// Function to start the game by clearing the wrapper, creating
-// and appending the buttons and all the cards to the DOM
+function removeDeck() {
+  while (cardsWrapper.firstChild) {
+    cardsWrapper.removeChild(cardsWrapper.firstChild);
+  }
+}
+// alg to shuffle array
+function arrShuffle(array) {
+  let place,
+      temp;
+  for (let i = array.length - 1; i > 0; i--) {
+    place = Math.floor(Math.random() * (i + 1));
+    temp = array[i];
+    array[i] = array[place];
+    array[place] = temp;
+  }
+  return array;
+};
+// func to pass cards through arrShuffle and create shuffled deck
+function shuffledCards() {
+  removeDeck();
+  const newCards = createCards();
+  const shuffled = arrShuffle(newCards);
+  createDecks(shuffled);
+}
+// func to remove shuffled deck and create a new arranged one
+function magic() {
+  removeDeck();
+  const deck = createCards();
+  createDecks(deck);
+}
+// removes start game btn and unhiddes btns
+function createButtons() {
+  const startGameBtn = document.getElementById('start-game');
+  startGameBtn.parentNode.removeChild(startGameBtn);
+  document.getElementById('btns').style.visibility = 'visible';
+}
+
+function events() {
+  document.getElementById('shuffle').addEventListener('click', shuffledCards);
+  document.getElementById('show-hide').addEventListener('click', hideShow);
+  document.getElementById('magic').addEventListener('click', magic);
+}
+
+// also creates 1st deck displaying cards in cards
 function startGame() {
   createButtons();
   createCards();
+  const firstDeck = createCards();
+  createDecks(firstDeck);
+  events();
 }
 
 document.getElementById('start-game').addEventListener('click', startGame);
+
